fa145a1471f10399554e0bb4387ddf22066ca68a
Bryant Vail <BryantVail@gmail.com>
2020-03-30 14:43:52 -0400

    if statements for whether a 'textContentFunction' was passed in

--- Sorting-Algos-and-Fund-DataStructures/03_UnderstandingQuickSort/LabReviewQuickSort.js
+++ Sorting-Algos-and-Fund-DataStructures/03_UnderstandingQuickSort/LabReviewQuickSort.js
@@ -9,56 +9,63 @@ let list = document.querySelector("#ctl04n0Nodes").querySelectorAll("table");
 let parentOfNodeList = document.querySelector("#ctl04n0Nodes");
 let parentOfNodeListId = "ctl04n0Nodes";
 
-SortAlgorithm.swap = function(list, swapIndex, index, innerHTMLFunction) {
+SortAlgorithm.swap = function(list, swapIndex, index, isInnerHTML) {
   //bigger value that's going to the back of the partition
   //-> goes into the temp value so the smaller number
   //-> can be moved to the front
-  var temp = innerHTMLFunction(list[swapIndex]);
-  innerHTMLFunction(list[swapIndex]) = innerHTMLFunction(list[index]);
-  innerHTMLFunction(list[index]) = temp;
+
+  var temp;
+
+  if (isInnerHTML) {
+    temp = list[swapIndex].innerHTML;
+    list[swapIndex].innerHTML = list[index].innerHTML;
+    list[index].innerHTML = temp;
+  } else {
+    temp = list[swapIndex];
+    list[swapIndex] = list[index];
+    list[index] = temp;
+  }
   return list;
 };
 
+//gets 4th index of 'td' and returns innerHTML
 UILabReview.getInnerHTML = function(listItemOfTheDOM) {
   return listItemOfTheDOM.querySelectorAll("td")[3].innerHTML;
 };
 
+//gets 4th index of 'td' and returns textContent
 UILabReview.getTextContent = function(listItemOfTheDOM) {
   return listItemOfTheDOM.querySelectorAll("td")[3].textContent;
 };
 
-UILabReview.getProviderValue = function(listItemOfTheDOM, delimiter) {
-  UILabReview.getTextContent(listItemOfTheDOM).slice(
+UILabReview.getProviderValue = function(listItemOfTheDOM) {
+  const delimiter = "(";
+  return UILabReview.getTextContent(listItemOfTheDOM).slice(
     UILabReview.getTextContent(listItemOfTheDOM).indexOf(delimiter)
   );
 };
 
 //creating the pivot
-SortAlgorithm.partitionLabReview = function(
-  array,
-  start,
-  end,
-  textContentFunction
-) {
-  //delimiter for combined text field
-  const delimiter = "(";
-
+// function passed in should return the value to be compared for sorting
+SortAlgorithm.partition = function(array, start, end, textContentFunction) {
   //determine pivot
-  const pivot = array[end]
-    .querySelectorAll("td")[3]
-    .textContent.slice(
-      array[end].querySelectorAll("td")[3].textContent.indexOf(delimiter)
-    );
+  let pivot;
+  if (textContentFunction) {
+    pivot = textContentFunction(array[end]);
+  } else {
+    pivot = array[end];
+  }
 
   let x = start - 1;
 
   for (let i = start; i < end; i++) {
-    //slice text to return content after delimiter
-    let comparisonValue = array[i]
-      .querySelectorAll("td")[3]
-      .textContent.slice(
-        array[i].querySelectorAll("td")[3].textContent.indexOf(delimiter)
-      );
+    let comparisonValue;
+    if (textContentFunction) {
+      //slice text to return content after delimiter
+      comparisonValue = textContentFunction(array[i]);
+    } else {
+      comparisonvalue = array[i];
+    }
 
     if (
       //access the 3rd "td" element within the "table" element of the array of "tables"
@@ -67,27 +74,46 @@ SortAlgorithm.partitionLabReview = function(
       x++;
       //put x in the place of array[i]
       //this ensures that all numbers less than the pivot
-      //-> are within indexes less than that of the pivot
-      SortAlgorithm.swap(array, x, i);
+      //-> are within indexes less than that of the
+      if (textContentFunction) {
+        SortAlgorithm.swap(array, x, i, true);
+      } else {
+        SortAlgorithm.swap(array, x, i);
+      }
     }
   }
   //put the pivot directly above the last value that
   //-> was lessThan the pivot
-  SortAlgorithm.swap(array, x + 1, end);
+  if (textContentFunction) {
+    SortAlgorithm.swap(array, x + 1, end, true);
+  } else {
+    SortAlgorithm.swap(array, x + 1, end);
+  }
   //return pivot position
   return x + 1;
 }; //end function
 
-SortAlgorithm.quickSortLabReview = function(array, start, end) {
+SortAlgorithm.quickSort = function(array, start, end, derivitiveFunction) {
   //if(array.length > 1){continue}
   //if(array.length == 1){start will == end}
   if (start < end) {
     //place partition in the middle
-    let partitionIndex = SortAlgorithm.partitionLabReview(array, start, end);
+    // pass in function if value needs to be derived from the array value
+    let partitionIndex = SortAlgorithm.partition(
+      array,
+      start,
+      end,
+      derivitiveFunction
+    );
     //sort beginning
-    SortAlgorithm.quickSortLabReview(array, start, partitionIndex - 1);
+    SortAlgorithm.quickSort(
+      array,
+      start,
+      partitionIndex - 1,
+      derivitiveFunction
+    );
     //sort end
-    SortAlgorithm.quickSortLabReview(array, partitionIndex + 1, end);
+    SortAlgorithm.quickSort(array, partitionIndex + 1, end, derivitiveFunction);
   }
   return array;
 };
@@ -115,14 +141,12 @@ UILabReview.sortLabReview = function(idOfParentNodeOfList, childNodeListType) {
   let parentNode = document.querySelector(`#${idOfParentNodeOfList}`);
   let childNodeList = parentNode.querySelectorAll(childNodeListType);
 
-  const sortedListOfNodes = SortAlgorithm.quickSortLabReview(
+  const sortedListOfNodes = SortAlgorithm.quickSort(
     childNodeList,
     0,
-    childNodeList.length - 1
+    childNodeList.length - 1,
+    UILabReview.getProviderValue
   );
-
-  // deleteChildren(parentNode);
-  // addChildren(parentNode, sortedListOfNodes);
 };
 
 // list.forEach(function(element) {
